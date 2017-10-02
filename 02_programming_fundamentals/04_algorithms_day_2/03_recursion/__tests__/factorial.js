aebb6d75e9ff804799cbb91079535d8371b129fe
Vincent Billey <vincent.billey@gmail.com>
2017-10-02 11:34:23 +0200

    add exercises and skeletton for algorithms day 2

---
+++ 02_programming_fundamentals/04_algorithms_day_2/03_recursion/__tests__/factorial.js
@@ -0,0 +1,14 @@
+const fact = require("../factorial");
+
+test("it should work", () => {
+  expect(fact(5)).toBe(120);
+  expect(fact(0)).toBe(1);
+});
+
+test("it should return null for negative numbers", () => {
+  expect(fact(-1)).toBe(null);
+});
+
+test("it should not work with a string", () => {
+  expect(fact("0")).toBe(null);
+});
---
+++ 02_programming_fundamentals/04_algorithms_day_2/03_recursion/fibonacci.js
@@ -0,0 +1,19 @@
+// In mathematics, the Fibonacci numbers are the numbers in the
+// following integer sequence, called the Fibonacci sequence,
+// and characterized by the fact that every number after
+// the first two is the sum of the two
+// preceding ones : 0, 1, 1, 2, 3, 5, 8, 13, 21...
+//
+// It does not work for negative numbers and your function
+// should return `null` if tried with a negative number or
+// with something else than a number.
+//
+// Remember that you can call `fibo` inside of itself
+// even several times
+
+function fibo(n) {
+  // your code here
+}
+
+// do not remove this line, it is for tests
+module.exports = fibo;
