39c598d7a066a418350680ea94d98e9c56be6d5d
Thierry Saintmaxent <thierry.saintmaxent@decathlon.com>
2017-11-23 13:12:39 +0100

    corriger les exercices 01 index 01 array 02 array method 01 can i drink

--- 02_programming_fundamentals/03_algorithms_day_1/02_play_with_arrays/02_arrays_methods.js
+++ 02_programming_fundamentals/03_algorithms_day_1/02_play_with_arrays/02_arrays_methods.js
@@ -3,12 +3,29 @@
  */
 
 // Rewrite your array `digits` using `push`;
+const digits = [];
+digits.push(0);
+digits.push(1);
+digits.push(2);
+digits.push(3);
+digits.push(4);
+digits.push(5);
+digits.push(6);
+digits.push(7);
+digits.push(8);
+digits.push(9);
 
+console.log(digits);
 
 // rewrite your variable `last` using `length`;
+const last = digits[digits.length-1];
 
-
+console.log(last);
 // create another array called `litteralDigits` from `zero` to `nine` where each array entry is a spelled-out number;
+const litteralDigits = ["zero","one","two","three","four","five","six","seven","eight","nine"];
+console.log(litteralDigits);
 
 
 // use `join` to create, into the variable `allDigits`, a string like this : `zero - one ...`.
+const allDigits = litteralDigits.join(" - ");
+console.log(allDigits);
--- 02_programming_fundamentals/03_algorithms_day_1/03_play_with_conditional_statement/01_can_i_drink_alcohol.js
+++ 02_programming_fundamentals/03_algorithms_day_1/03_play_with_conditional_statement/01_can_i_drink_alcohol.js
@@ -1,5 +1,4 @@
 // > Frieda and Francis walk on the street. Frieda is 22 and Francis is 17.
-//
 // Create two objects, `francis`, `frieda` with the fields :
 //   `age` (an integer) and `name`, (obviously a string)
 
@@ -14,3 +13,21 @@
 // it is expected to see failures for these tests:
 // "Frieda|Francis should be inside an object frieda|francis".
 // All the other tests should work.
+const francis = {
+  age: 17,
+  name: "Francis"
+};
+
+const frieda = {
+  age: 22,
+  name: "Frieda"
+};
+
+// Create two variables `canFriedaDrinkAlcohol` and `canFrancisDrinkAlcohol` to answer, respectively, to the question "Can he / she drink
+// alcohol.".
+
+// These variables should stay correct if we modify the ages of the objects `francis` and `frieda` in the declaration.
+
+// These variables should stay correct if we modify the ages of the objects `francis` and `frieda`.
+const canFriedaDrinkAlcohol= true;
+const canFrancisDrinkAlcohol= true;
