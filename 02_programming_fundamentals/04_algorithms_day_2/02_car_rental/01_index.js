7cd4f66b16d0a55c58d00eafbd5da1953d6f9389
Kevin Disneur <kevin@disneur.me>
2017-11-24 09:51:26 +0100

    Add "Rent a Car as a function" skeleton

---
+++ 02_programming_fundamentals/04_algorithms_day_2/02_car_rental/01_index.js
@@ -0,0 +1,17 @@
+const driver = {
+  driverLicense: "B1",
+  licenseIssued: new Date().getFullYear() - 10, // 10 years old license
+  numberOfAccident: 0,
+  bonus: 0.8,
+};
+
+// Write a function canRentACar:
+// * Input: a driver
+// * Output: a boolean if the driver can rent a car
+
+
+
+
+// ⚠ Do not remove me ! It's for tests
+// eslint-disable-next-line
+module.exports = canRentACar
