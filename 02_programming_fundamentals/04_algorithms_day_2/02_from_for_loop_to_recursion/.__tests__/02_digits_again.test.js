0a35358acd027d0aee25cd46593fbadf027fa045
Kevin Disneur <kevin@disneur.me>
2017-11-24 11:51:08 +0100

    Rename/Delete recursion folder

--- 02_programming_fundamentals/04_algorithms_day_2/02_from_for_loop_to_recursion/.__tests__/02_digits_again.test.js
+++
@@ -1,7 +0,0 @@
-/* global beforeAll test expect */
-const makeDigits = require('../02_digits_again')
-
-test("digits exists", () => {
-  const digits = makeDigits(0, []);
-  expect(digits).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
-});
