bc02a590f700dbfed94a715ffb112bc16dcae08f
Thierry Saintmaxent <thierry.saintmaxent@decathlon.com>
2017-11-24 13:58:09 +0100

    Day 2 exo 1

--- 02_programming_fundamentals/04_algorithms_day_2/01_range_function/01_range.js
+++ 02_programming_fundamentals/04_algorithms_day_2/01_range_function/01_range.js
@@ -4,10 +4,27 @@
 // This function should work in both ascending or descending order.
 
 // Complete this function.
-function range() {
 
+
+function range(start,end)
+{
+  let resultat=[];
+  if (end >= start) {
+    for (let i=start;i <= end;i++) {
+      resultat.push(i);
+    }
+    return resultat;
+  }
+  if (start > end) {
+    for (let i=start;i >= end;i--) {
+      resultat.push(i);
+    }
+    return resultat;
+  }
 }
 
+
+console.log(range(4,1));
 // Do not remove last lines, it is for tests
 // eslint-disable-next-line
 module.exports = range;
