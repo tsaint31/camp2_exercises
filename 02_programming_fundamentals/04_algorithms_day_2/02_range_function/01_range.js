30bb1da1eb70f9699a2f752a2aac3977f97c1d29
arnaudmorisset <arnaudmorisset@protonmail.com>
2017-11-24 09:44:22 +0100

    feat: add range function exercise

---
+++ 02_programming_fundamentals/04_algorithms_day_2/02_range_function/01_range.js
@@ -0,0 +1,13 @@
+// Write a function that takes two input parameters, and returns a new array with defaults values between the start
+// number and the end number.
+//
+// This function should work in both ascending or descending order.
+
+// Complete this function.
+function range() {
+
+}
+
+// Do not remove last lines, it is for tests
+// eslint-disable-next-line
+module.exports = range;
