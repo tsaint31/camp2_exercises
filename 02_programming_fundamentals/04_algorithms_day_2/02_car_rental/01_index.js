d22892562f55ae40e542c783561ccda72f28cd4a
Thierry Saintmaxent <thierry.saintmaxent@decathlon.com>
2017-11-24 14:29:40 +0100

    day 2 exo 2

--- 02_programming_fundamentals/04_algorithms_day_2/02_car_rental/01_index.js
+++ 02_programming_fundamentals/04_algorithms_day_2/02_car_rental/01_index.js
@@ -1,15 +1,26 @@
 const driver = {
-  driverLicense: "B1",
-  licenseIssued: new Date().getFullYear() - 10, // 10 years old license
+  driverLicense: "B",
+  licenseIssued: new Date().getFullYear() - 2, // 10 years old license
   numberOfAccident: 0,
-  bonus: 0.8,
+  bonus: 0.3,
 };
 
 // Write a function canRentACar:
 // * Input: a driver
 // * Output: a boolean if the driver can rent a car
 
+function canRentACar(driver) {
 
+  if (driver.driverLicense.startsWith("B") &&
+   driver.licenseIssued <=(new Date().getFullYear() - 2) &&
+    (driver.numberOfAccident === 0 || driver.bonus >= 0.7 ))
+  {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(canRentACar(driver));
 
 
 // ⚠ Do not remove me ! It's for tests
