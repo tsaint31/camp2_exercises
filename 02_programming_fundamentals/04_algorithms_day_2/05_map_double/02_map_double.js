f803b2df0cc028ffedcbda6c1cbbd43cec6ccb01
arnaudmorisset <arnaudmorisset@protonmail.com>
2017-11-24 10:56:48 +0100

    feat: add map double exercises

---
+++ 02_programming_fundamentals/04_algorithms_day_2/05_map_double/02_map_double.js
@@ -0,0 +1,17 @@
+// Create a function double which take an integer and return the double of the integer.
+// Then, try to create a function called map. This function should take two parameters:
+//
+// - an array
+// - a function
+//
+// this function apply the function passed as a parameter on each values of the array (also passed as a parameter).
+//
+// Finally, test map with an array of your choice and the double function.
+//
+// WARNING: You're not allowed to use `Array.map`!
+
+// Your code here...
+
+// Do not remove last lines, it is for tests
+// eslint-disable-next-line
+module.exports = { double: double, map: map };
