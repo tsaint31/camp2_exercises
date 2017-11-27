8992685d0835ad56de7f51b5fcafd72541d243d8
Thierry Saintmaxent <thierry.saintmaxent@decathlon.com>
2017-11-27 13:11:47 +0100

    week 2 day 1 exo 3

--- 02_programming_fundamentals/05_callbacks/03_square_digits/square_digits.js
+++ 02_programming_fundamentals/05_callbacks/03_square_digits/square_digits.js
@@ -7,9 +7,23 @@
 // Note: This is not just the square of a number but the square of each digits
 // Note: The function accepts an integer and returns an integer
 
+const square = function (x) {
+  return x * x;
+};
+
+
+
 function squareDigits(number) {
   // Your code here
+  const caract= `${number}`;
+  const result=caract.split("");
+  const resultat=result.map(square);
+  const resultat1 = resultat.join("");
+  return (parseInt(resultat1,10));
+
 }
+
+console.log(squareDigits(9129));
 
 // Do not remove last lines, it is for tests
 // eslint-disable-next-line
