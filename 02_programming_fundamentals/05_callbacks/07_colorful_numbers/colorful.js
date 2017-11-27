65a6261ab713737a78f659582cb1041ae7faf2b8
Thierry Saintmaxent <thierry.saintmaxent@decathlon.com>
2017-11-27 17:10:56 +0100

    week 2 day 1 exo 7

--- 02_programming_fundamentals/05_callbacks/07_colorful_numbers/colorful.js
+++ 02_programming_fundamentals/05_callbacks/07_colorful_numbers/colorful.js
@@ -11,6 +11,39 @@
 // Insert your code here ⇩
 
 
+function each_item(array) {
+  const result_each_item=[];
+  for (let j=0; j<array.length;j++) {
+    let value=1;
+    for (let i=j; i<array.length;i++) {
+      value = array[i]*value;
+      result_each_item.push(value);
+    }
+  }
+  return result_each_item;
+}
+
+
+function isColorful(number) {
+  const caract= `${number}`;
+  const result=caract.split("");
+  const resultat = each_item(result);
+  for (let i=0; i<resultat.length;i++) {
+    for (let j=i+1; j<resultat.length;j++) {
+      if(resultat[i]===resultat[j]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+
+isColorful(10);
+
+
+
+
 // Do not remove last lines, it is for tests
 // eslint-disable-next-line
 module.exports = isColorful;
