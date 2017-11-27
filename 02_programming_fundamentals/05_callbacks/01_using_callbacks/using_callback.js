bb69012bf743a85fc34654581c57b0907dc5a456
Thierry Saintmaxent <thierry.saintmaxent@decathlon.com>
2017-11-27 11:48:50 +0100

    week 2 day 1 exo 1

--- 02_programming_fundamentals/05_callbacks/01_using_callbacks/using_callback.js
+++ 02_programming_fundamentals/05_callbacks/01_using_callbacks/using_callback.js
@@ -1,8 +1,16 @@
+
+
+
 const thisIsThePlayer = (callback) => {
   const player = { name: "Spartacus", life: 100, strength: 100, agility: 100 };
   callback(player);
-}
+};
 
 // Write a function sayHello that greets: "Hello, Spartacus". (Where "Spartacus" is the player name)
+
+function sayHello(joueur) {
+  console.log(`Hello, ${joueur.name}`);
+}
+
 
 thisIsThePlayer(sayHello);
