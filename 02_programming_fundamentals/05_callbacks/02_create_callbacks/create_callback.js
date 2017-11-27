719cb1912e186f1bd0fa1781b841b3f57bb7449d
Thierry Saintmaxent <thierry.saintmaxent@decathlon.com>
2017-11-27 11:55:49 +0100

    week 2 day 1 exo 2

--- 02_programming_fundamentals/05_callbacks/02_create_callbacks/create_callback.js
+++ 02_programming_fundamentals/05_callbacks/02_create_callbacks/create_callback.js
@@ -2,8 +2,11 @@ const log = (text) => console.log(text);
 
 // A function that takes two parameters, this first one being an object like below
 // and the second one being a callback function.
-const getInput = (____, ____) => {
+const getInput = (obj, func) => {
   // When finished, we want to log the key params...
+  func(obj.key);
 };
+
+
 
 getInput({user: "Lorem", key: "arrow_up"}, log);
