fa73755b95eb5433656ffc78ea4fb51c010cae84
Kevin Disneur <kevin@disneur.me>
2017-11-27 11:07:47 +0100

    Add colorful exercise

---
+++ 02_programming_fundamentals/05_callbacks/07_colorful_numbers/colorful.js
@@ -0,0 +1,16 @@
+// Create a function `isColorful(number)` that will return `true` if the given number is colorful
+// or `false` if the given number is not colorful
+//
+// Example:
+//
+// isColorful(3245) # => true
+// isColorful(10) # => false
+//
+// Note: Read Sparta exercises to have more details about what defines a colorful number
+
+// Insert your code here ⇩
+
+
+// Do not remove last lines, it is for tests
+// eslint-disable-next-line
+module.exports = isColorful;
