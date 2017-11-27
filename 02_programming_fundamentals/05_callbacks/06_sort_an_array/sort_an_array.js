6beb87929e45f143c5e71f4c509168ee83f56c8b
Thierry Saintmaxent <thierry.saintmaxent@decathlon.com>
2017-11-27 16:05:38 +0100

    week 2 day 1 exo 6

--- 02_programming_fundamentals/05_callbacks/06_sort_an_array/sort_an_array.js
+++ 02_programming_fundamentals/05_callbacks/06_sort_an_array/sort_an_array.js
@@ -8,8 +8,31 @@
 
 function sort(unsortedArray) {
   // Your code here
-}
+  const result=[];
+  let done;
+  const done_table=[];
 
+  for (let j=0; j<unsortedArray.length; j++) {
+    let value=unsortedArray[0];
+    for (let i=0; i<unsortedArray.length; i++) {
+      if (done_table.indexOf(i)=== -1) {
+        if (done_table.length===unsortedArray.length-1) {
+          value=unsortedArray[i];
+        }
+        else if (value >= unsortedArray[i]) {
+          value=unsortedArray[i];
+          done=i;
+        }
+      }
+    }
+    done_table.push(done);
+    console.log(done_table);
+    result.push(value);
+    console.log(result);
+  }
+  return result;
+}
+sort([24, 7, 9, 72, -8]);
 // Do not remove last lines, it is for tests
 // eslint-disable-next-line
 module.exports = sort;
