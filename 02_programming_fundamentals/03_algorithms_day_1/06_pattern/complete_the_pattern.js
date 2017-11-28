1b586d49387ceb818ea86133a717ea8961c5667a
Thierry Saintmaxent <thierry.saintmaxent@decathlon.com>
2017-11-28 18:24:11 +0100

    week 2 day 2 exo pattern

--- 02_programming_fundamentals/03_algorithms_day_1/06_pattern/complete_the_pattern.js
+++ 02_programming_fundamentals/03_algorithms_day_1/06_pattern/complete_the_pattern.js
@@ -1,3 +1,21 @@
 // Modify this value to test with other values
-const numberOfLine = 5;
+const numberOfLine = 4;
 // Your code here ⬇
+const resultat=[];
+let line="";
+let caract="";
+let temp;
+
+
+for (let i=0; i<numberOfLine;i++) {
+  for (let j=0;j<numberOfLine-i;j++) {
+    temp=numberOfLine-j;
+    caract= `${temp}`;
+    line=line+temp;
+  }
+  resultat.push(line);
+  line="";
+}
+for (let i=0;i<resultat.length;i++) {
+  console.log(resultat[i]);
+}
