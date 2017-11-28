2230c1850264ba4f8d31f17a61fdb1a80658160a
Thierry Saintmaxent <thierry.saintmaxent@decathlon.com>
2017-11-28 13:31:46 +0100

    week 2 day 2 exo1 et 2

--- 02_programming_fundamentals/06_your_first_program/01_number_game/number_game.js
+++ 02_programming_fundamentals/06_your_first_program/01_number_game/number_game.js
@@ -6,30 +6,33 @@ const reader = readline.createInterface({
 });
 
 const number_to_find=Math.ceil(Math.random(100)*100);
+console.log(number_to_find);
 
 function askQuestion() {
-  reader.question("What is your number? ", value => {
-    if (isNaN(parseInt(value,10))) {
-      console.log("Your number is not a number");
-      askQuestion();
-    }
-    else if (parseInt(value,10)<1 || parseInt(value,10)>100) {
-      console.log("The number is between 1 and 100");
-      askQuestion();
-    }
-    else if (parseInt(value,10)===number_to_find) {
-      console.log("You won!");
-      reader.close();
-    }
-    else if (parseInt(value,10)>number_to_find) {
-      console.log("Too high");
-      askQuestion();
-    }
-    else {
-      console.log("Too Low");
-      askQuestion();
-    }
-  });
+reader.question("What is your number? ", value => {
+  console.log(value);
+  console.log(number_to_find);
+  if (isNaN(parseInt(value))) {
+    console.log(`Your number is not a number`);
+    askQuestion()
+   }
+   else if (parseInt(value)<1 || parseInt(value)>100) {
+     console.log("The number is between 1 and 100");
+     askQuestion();
+   }
+  else if (parseInt(value)===number_to_find) {
+    console.log("You won!");
+    reader.close();
+  }
+  else if (parseInt(value)>number_to_find) {
+    console.log("Too high");
+    askQuestion();
+  }
+  else {
+    console.log("Too Low");
+    askQuestion();
+  }
+});
 }
 
 askQuestion();
--- 02_programming_fundamentals/06_your_first_program/02_number_game_with_stats/number_game.js
+++ 02_programming_fundamentals/06_your_first_program/02_number_game_with_stats/number_game.js
@@ -7,40 +7,41 @@ const reader = readline.createInterface({
 
 
 const number_to_find=Math.ceil(Math.random(100)*100);
+console.log(number_to_find);
 let numberTry=1;
 
 
 function askQuestion() {
-  reader.question("What is your number? ", value => {
-    if (isNaN(parseInt(value,10))) {
-      console.log("Your number is not a number");
-      numberTry=numberTry+1;
-      askQuestion();
-    }
-    else if (parseInt(value,10)<1 || parseInt(value,10)>100) {
-      console.log("The number is between 1 and 100");
-      numberTry=numberTry+1;
-      askQuestion();
-    }
-    else if (parseInt(value,10)===number_to_find) {
-      if (numberTry===1) {
-        console.log("You won in one shot!");
-        reader.close();}
-      else {console.log(`You won in ${numberTry} tries`);
-        reader.close();}
-    }
+reader.question("What is your number? ", value => {
+  if (isNaN(parseInt(value))) {
+    console.log(`Your number is not a number`);
+    numberTry=numberTry+1;
+    askQuestion();
+   }
+   else if (parseInt(value)<1 || parseInt(value)>100) {
+     console.log("The number is between 1 and 100");
+     numberTry=numberTry+1;
+     askQuestion();
+   }
+  else if (parseInt(value)===number_to_find) {
+    if (numberTry===1) {
+    console.log("You won in one shot!");
+    reader.close();}
+    else {console.log(`You won in ${numberTry} tries`);
+    reader.close();}
+  }
 
-    else if (parseInt(value,10)>number_to_find) {
-      console.log("Too high");
-      numberTry=numberTry+1;
-      askQuestion();
-    }
-    else {
-      console.log("Too Low");
-      numberTry=numberTry+1;
-      askQuestion();
-    }
-  });
+  else if (parseInt(value)>number_to_find) {
+    console.log("Too high");
+    numberTry=numberTry+1;
+    askQuestion();
+  }
+  else {
+    console.log("Too Low");
+    numberTry=numberTry+1;
+    askQuestion();
+  }
+});
 }
 
 askQuestion();
