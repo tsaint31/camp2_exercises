1369817a11a664e114539ce32892955fb79d9ad2
Thierry Saintmaxent <thierry.saintmaxent@decathlon.com>
2017-11-28 16:07:41 +0100

    week 2 day 1 exo 3

--- 02_programming_fundamentals/06_your_first_program/03_memory/memory.js
+++ 02_programming_fundamentals/06_your_first_program/03_memory/memory.js
@@ -1,2 +1,68 @@
 // This function will clear the terminal when called
 const clear = require("cli-clear");
+
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const cards = ["🐰", "🐰", "🎃", "🎃", "🌲","🌲"];
+let mask_cards = ["1","2","3","4","5","6"];
+let number_cards=0;
+let couple=[];
+let win=[];
+let resultat=[];
+
+function already_find(valeur) {
+  for (let k=0;k<win.length;k++) {
+    if (valeur===win[k]) {
+      return true;
+    }
+  }
+  return false;
+
+}
+
+function askQuestion() {
+  reader.question("Please select one card? ", value => {
+    couple.push(value);
+    for (let i=0;i<cards.length;i++) {
+      if (i===couple[0]-1 || i===couple[1]-1 ) {
+        resultat.push(cards[i]);}
+      else if (already_find(i)) {
+        resultat.push(cards[i]);
+      }
+      else {resultat.push(mask_cards[i]);}
+    }
+    if (cards[couple[0]-1]===cards[couple[1]-1]) {
+      win.push(couple[0]-1);
+      win.push(couple[1]-1);
+      console.log(win);
+    }
+    clear();
+    console.log(resultat);
+    resultat=[];
+    number_cards=number_cards+1;
+    if (number_cards!==2) {
+      askQuestion();}
+    else {
+      if (win.length===cards.length) {
+        console.log("YOUUUUUU WIN");
+        reader.close();
+      }
+      else {
+        number_cards=0;
+        couple=[];
+        askQuestion();}
+    }});
+}
+
+
+function card_table(table) {
+  console.log(mask_cards);
+  askQuestion();
+}
+
+card_table(cards);
