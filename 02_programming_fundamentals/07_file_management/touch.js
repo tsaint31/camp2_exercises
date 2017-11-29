d81b6bf60c5604339e01ebb90666cdf1b9a7278c
Thierry Saintmaxent <thierry.saintmaxent@decathlon.com>
2017-11-29 14:41:38 +0100

    week 2 day 3 exo 7

--- 02_programming_fundamentals/07_file_management/touch.js
+++ 02_programming_fundamentals/07_file_management/touch.js
@@ -1,13 +1,35 @@
 const fs = require('fs');
 
 
-function touch(file) {
-  fs.open(file, 'w', (error, file) => {
+function creation(file) {
+  fs.open(file, "w", (error, file) => {
     if (error) {
       return console.warn(error);
     }
-    console.log('Saved!');
   });
 }
 
-touch("source_TSM.txt")
+function touch(file) {
+  fs.access(file, (error) => {
+    if (error) {
+      creation(file);
+    }
+    else {
+      fs.open(file, "r", (error, fd) => {
+        if (error) {
+          return console.warn(error);
+        }
+        fs.futimes(fd,new Date(),new Date(),(error) => {
+          if (error) {
+            return console.warn(error);
+          }
+        });
+      });
+    }
+  });
+}
+
+
+
+touch("source3_TSM.txt");
+// touch("source_TSM.txt")
