35beaa840a8df6dd95412b5735019a3c0614f76e
Thierry Saintmaxent <thierry.saintmaxent@decathlon.com>
2017-11-30 15:11:38 +0100

    week2 day 4 exo orange

--- 02_programming_fundamentals/08_this_day_1/04_bonus/04_orange_tree.js
+++ 02_programming_fundamentals/08_this_day_1/04_bonus/04_orange_tree.js
@@ -24,31 +24,25 @@ const orangeTree = {
   },
 
   ageOneYear: function() {
-    if (this.age < 99) {
-      this.age=this.age+1;
-      if (this.age <5) {
-        this.height=this.height+25;
-      }
-      else if (this.age>=5 && this.age <10) {
-        this.oranges=10;
-        this.height=this.height+25;
-      }
-      else if (this.age>=10 && this.age <20) {
-        this.oranges=20;
-        this.height=this.height+10;
-      }
-      else if (this.age>=20 && this.age <40) {
-        this.oranges=5;
-      }
-      else
-      {
-        this.oranges=0;
-        if(this.age>=50){
-          const ageDie=Math.floor(Math.random()*(100-this.age)+this.age);
-          if (ageDie===this.age || this.age===99) {
-            this.alive=false;
-          }
-        }
+    this.age=this.age+1;
+    if (this.age <5) {
+      this.height=this.height+25;
+    }
+    else if (this.age>=5 && this.age <10) {
+      this.oranges=10;
+      this.height=this.height+25;
+    }
+    else if (this.age>=10 && this.age <20) {
+      this.oranges=20;
+      this.height=this.height+10;
+    }
+    else if (this.age>=20 && this.age <40) {
+      this.oranges=5;
+    }
+    else if (this.age>=40) {
+      this.oranges=0;
+      if ((this.age>=50 && ((Math.random()+(this.age/200))>0.5)) || this.age===100) {
+        this.alive=false;
       }
     }
   }
@@ -56,7 +50,8 @@ const orangeTree = {
 //should be able to pick an orange when there are some'
 
 orangeTree.seed();
-for (let i=0;i<100;i++) {
+for (let i=0;i<50;i++) {
   orangeTree.ageOneYear();
 }
+console.log(orangeTree.alive);
 module.exports = orangeTree;
