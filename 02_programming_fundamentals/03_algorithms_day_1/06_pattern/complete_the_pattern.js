4157b20ca579a5be648aa0f2910e6fe532c3af0f
Thierry Saintmaxent <thierry.saintmaxent@decathlon.com>
2017-11-30 17:20:22 +0100

    week 2 day 4 exo bonus

--- 02_programming_fundamentals/03_algorithms_day_1/06_pattern/complete_the_pattern.js
+++ 02_programming_fundamentals/03_algorithms_day_1/06_pattern/complete_the_pattern.js
@@ -1,10 +1,11 @@
 // Modify this value to test with other values
-const numberOfLine = 4;
+const numberOfLine = 8;
 // Your code here ⬇
 const resultat=[];
 let line="";
 let caract="";
 let temp;
+let result="";
 
 
 for (let i=0; i<numberOfLine;i++) {
@@ -17,5 +18,6 @@ for (let i=0; i<numberOfLine;i++) {
   line="";
 }
 for (let i=0;i<resultat.length;i++) {
-  console.log(resultat[i]);
+  result=result+`${resultat[i]}\n`;
 }
+console.log(result);
