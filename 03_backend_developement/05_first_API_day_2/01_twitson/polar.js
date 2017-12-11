98ffb6ab8469449549ab11d228673c9fd700c07e
thierry saintmaxent <thierry.saintmaxent@decathlon.com>
2017-12-11 11:38:16 +0100

    W4 D1 polar

--- 03_backend_developement/05_first_API_day_2/01_twitson/polar.js
+++ 03_backend_developement/05_first_API_day_2/01_twitson/polar.js
@@ -1,71 +1,24 @@
+const oauth = require("OAuth");
+const OAuth = new oauth.OAuth(
+  process.env.POLAR_CLIENT_ID,
+  process.env.POLAR_CLIENT_SECRET
+);
 
-const request = require("request");
-const express = require("express");
-const fetch = require("node-fetch");
+const url = encodeURI("https://flow.polar.com/oauth2/authorization?response_type=code&client_id=");
 
-const app = express();
-const port = process.env.PORT || 3000;
-const POLAR_CLIENT_ID=process.env.POLAR_CLIENT_ID;
-const POLAR_CLIENT_SECRET=process.env.POLAR_CLIENT_SECRET;
+// https://flow.polar.com/oauth2/authorization?response_type=code&scope={SCOPE}&client_id={CLIENT_ID}&state={STATE}
 
-app.listen(port, function () {
-  console.log("Server listening on port:" + port);
-});
 
-// https://flow.polar.com/oauth2/authorization?response_type=codeclient_id=
+function retrieveTweets(callback) {
+  OAuth.get(url+process.env.POLAR_CLIENT_ID, function (error, data) {
+    const text=JSON.parse(data);
+    console.log(text);
+    callback(text);
 
-app.get("/", function(request, result) {
-  const code = request.param("code");
-  console.log(code);
-  getToken(code);
-});
-
-function getToken(code){
-  const clientSecretEncoded = new Buffer(POLAR_CLIENT_ID + ":" +POLAR_CLIENT_SECRET).toString("base64");
-  const headers = {"Authorization": `Basic ${clientSecretEncoded}`};
-  request(
-    {
-      url: "https://polarremote.com/v2/oauth2/token",
-      method: "POST",
-      headers: headers,
-      form: {"grant_type" : "authorization_code",
-        "code" : `${code}`
-      }
-    },
-    function(error, response, result) {
-      const Token= JSON.parse(result).access_token;
-      console.log( `token à utiliser = ${Token}`);
-      registerUser(Token);
-    }
-  );
+  });
 }
-// https://flow.polar.com/oauth2/authorization?response_type=code&client_id=
-
-function registerUser(token) {
-  // const inputBody = {
-  //   "member-id": "User_id_999"
-  // };
-  const headers = {
-    "Content-Type":"application/xml",
-    "Accept":"application/json",
-    "Authorization": `Bearer ${token}`
-  };
-
-  console.log("token" + token);
-  fetch("https://www.polaraccesslink.com/v3/users",
-    {
-      method: "POST",
-      body: {"member-id" : "User_id_999"},
-      headers: headers
-    })
-    .then(function(res) {
-      console.log(res);
-      return res.json();
-    })
-    .then(function(body) {
-      console.log(body);
-    })
-    .catch((error) => {
-      console.warn(error);
-    });
+function callback(text) {
+  console.log(text);
 }
+
+retrieveTweets(callback);
