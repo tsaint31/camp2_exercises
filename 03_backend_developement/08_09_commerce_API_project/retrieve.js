208770d816d28f5d917774ff576e3a83f154d191
thierry saintmaxent <thierry.saintmaxent@decathlon.com>
2017-12-13 18:45:30 +0100

    commerce API

--- 03_backend_developement/08_09_commerce_API_project/retrieve.js
+++ 03_backend_developement/08_09_commerce_API_project/retrieve.js
@@ -1,72 +1,143 @@
-const insertion = require("./insertion");
-const request = require("request");
+
 const { Pool } = require("pg");
 const pool = new Pool();
+const request = require("request");
 
-function retrieve(callback,data) {
+function retrieveCategories() {
   request(
     {
-      url: `https://decath-product-api.herokuapp.com/${data}`,
+      url: "https://decath-product-api.herokuapp.com/categories",
       method: "GET"
     },
     function (error,response,body) {
       const result=JSON.parse(body);
-      callback(result,data);
+      // console.log(result);
+      pool.connect((err, client,callback) => {
+        if (err) throw err;
+        for (let i=0;i<result.length;i++) {
+          client.query("INSERT INTO categories (id,decathlon_id,label) VALUES ($1::uuid,$2::integer,$3::text);",
+            [result[i].id,result[i].decathlon_id,result[i].label], (err, res) => {
+              if (err) {
+                // console.log(err.stack);
+              } else {
+                // console.log(res.rows[0]);
+              }
+            });
+        }
+        callback();
+      });
     }
   );
 }
 
-function retrieveCatProd(categ,index,callback) {
-  if (index<categ.length) {
-    request(
-      {
-        url: `https://decath-product-api.herokuapp.com/categories/${categ[index].id}/products`,
-        method: "GET"
-      },
-      function (error,response,body) {
-        const result=JSON.parse(body);
-        callback(result,"category_products",categ,index);
-      }
-    );
-  }
-}
-
-function insertionCatProd(result,data,categ,index){
-  pool.connect((err, client,done) => {
-    if (err) throw err;
-    for (let i=0;i<result.length;i++) {
-      client.query(`INSERT INTO category_products (id_product,id_category) VALUES ($1::uuid,'${categ[index].id}');`,
-        [result[i].id], (err, res) => {
-          if (err) {
-            // console.log(err.stack);
-          }
-        });
+function retrieveBrands() {
+  request(
+    {
+      url: "https://decath-product-api.herokuapp.com/brands",
+      method: "GET"
+    },
+    function (error,response,body) {
+      const result=JSON.parse(body);
+      // console.log(result);
+      pool.connect((err, client,callback) => {
+        if (err) throw err;
+        for (let i=0;i<result.length;i++) {
+          client.query("INSERT INTO brands (id,title) VALUES ($1::uuid,$2::text);",
+            [result[i].id,result[i].title], (err, res) => {
+              if (err) {
+                // console.log(err.stack);
+              } else {
+                // console.log(res.rows[0]);
+              }
+            });
+        }
+        callback();
+      });
     }
-    retrieveCatProd(categ,index+1,insertionCatProd);
-    done();
-  });
+  );
 }
 
+function retrieveProducts() {
+  request(
+    {
+      url: "https://decath-product-api.herokuapp.com/products",
+      method: "GET"
+    },
+    function (error,response,body) {
+      const result=JSON.parse(body);
+      console.log(result);
+      pool.connect((err, client,callback) => {
+        if (err) throw err;
+        for (let i=0;i<result.length;i++) {
+          client.query("INSERT INTO products (id,decathlon_id,title,description,brand_id,min_price,max_price,crossed_price,percent_reduction,image_path,rating) VALUES ($1::uuid,$2::integer,$3::text,$4::text,$5::uuid,$6::float,$7::float,$8::float,$9::float,$10::text,$11::float);",
+            [result[i].id,result[i].decathlon_id,result[i].title,result[i].description,result[i].brand_id,result[i].min_price,result[i].max_price,result[i].crossed_price,result[i].percent_reduction,result[i].image_path,result[i].rating], (err, res) => {
+              if (err) {
+                console.log(err);
+              } else {
+                // console.log(res.rows[0]);
+              }
+            });
+        }
+        callback();
+      });
+    }
+  );
+}
+
+function retrieveCategoryProducts() {
+  request(
+    {
+      url: "https://decath-product-api.herokuapp.com/categories",
+      method: "GET"
+    },
+    function (error,response,body) {
+      const result=JSON.parse(body);
+      for (let j=500;j<1002;j++) {
+        const cat=result[j].id;
+        console.log(`cat:  ${cat}`);
+        request(
+          {
+            url: `https://decath-product-api.herokuapp.com/categories/${cat}/products`,
+            method: "GET"
+          },
+          function (error,response,body) {
+            const result=JSON.parse(body);
+            if (error) {
+              console.log(error);
+            }
+            else {
+              pool.connect((err, client,callback) => {
+                if (err) throw err;
+                for (let i=0;i<result.length;i++) {
+                  client.query(`INSERT INTO category_products (id_product,id_category) VALUES ($1::uuid,'${cat}');`,
+                    [result[i].id], (err, res) => {
+                      if (err) {
+                        console.log(err.stack);
+                      } else {
+                        console.log(res.rows[0]);
+                      }
+                    });
+                }
+                callback();
+              });
+            }
+          }
+        );
+      }
+    }
+  );
+}
 
 function retrieveData() {
-  retrieve(insertion.insertionData,"categories");
-  retrieve(insertion.insertionData,"brands");
-  retrieve(insertion.insertionData,"products");
-  retrieve(CatProd,"categories");
+  retrieveCategories();
+  retrieveBrands();
+  retrieveProducts();
+
+  retrieveCategoryProducts();
 }
 
-function CatProd(result) {
-  const categ=result;
-  const index=0;
-  retrieveCatProd(categ,index,insertionCatProd);
+function callback() {
+  pool.end();
 }
 
-
-retrieveData();
-
-
-module.exports = {
-  retrieve: retrieve,
-  retrieveCatProd: retrieveCatProd,
-  retrieveData: retrieveData
-};
+retrieveData(callback);
--- 03_backend_developement/08_09_commerce_API_project/server.js
+++ 03_backend_developement/08_09_commerce_API_project/server.js
@@ -1,37 +1,110 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const query = require("./query");
+const PG = require("pg");
+const client = new PG.Client();
 
-function applisten() {
-  app.listen(port, function () {
-    console.log("Server listening on port:" + port);
-  });
-}
+app.listen(port, function () {
+  client.connect();
+  console.log("Server listening on port:" + port);
+});
 
-function getData(data,callback) {
-  app.get(`/${data}`, function(request, result) {
-    callback(result,data);
-  });
-}
+app.get("/categories", function(request, result) {
+  client.query(
+    "SELECT * FROM categories ",
+    [],
+    function(error, result1) {
+      if (error) {
+        console.warn(error);
+      } else {
+        result.json(result1.rows);
+      }
+    }
+  );
+});
 
-function getDataID (data,callback) {
-  app.get(`/${data}/:id`, function(request, result) {
-    callback(request,result,data);
-  });
-}
+app.get("/categories/:id", function(request, result) {
+  client.query(
+    "SELECT * FROM categories where id = $1::uuid",
+    [request.params.id],
+    function(error, result1) {
+      if (error) {
+        console.warn(error);
+      } else {
+        result.json(result1.rows);
+      }
+    }
+  );
+});
 
-function getCatProd (callback) {
-  app.get("/categories/:id/products", function(request, result) {
-    callback(request,result);
-  });
-}
+app.get("/brands", function(request, result) {
+  client.query(
+    "SELECT * FROM brands ",
+    [],
+    function(error, result1) {
+      if (error) {
+        console.warn(error);
+      } else {
+        result.json(result1.rows);
+      }
+    }
+  );
+});
 
-applisten();
-getData("categories",query.queryData);
-getData("brands",query.queryData);
-getData("products",query.queryData);
-getDataID("categories",query.queryDataID);
-getDataID("brands",query.queryDataID);
-getDataID("products",query.queryDataID);
-getCatProd(query.queryCatProd);
+app.get("/brands/:id", function(request, result) {
+  client.query(
+    "SELECT * FROM brands where id = $1::uuid",
+    [request.params.id],
+    function(error, result1) {
+      if (error) {
+        console.warn(error);
+      } else {
+        result.json(result1.rows);
+      }
+    }
+  );
+});
+
+app.get("/products", function(request, result) {
+  client.query(
+    "SELECT * FROM products ",
+    [],
+    function(error, result1) {
+      if (error) {
+        console.warn(error);
+      } else {
+        result.json(result1.rows);
+      }
+    }
+  );
+});
+
+app.get("/products/:id", function(request, result) {
+  client.query(
+    "SELECT * FROM products where id = $1::uuid",
+    [request.params.id],
+    function(error, result1) {
+      if (error) {
+        console.warn(error);
+      } else {
+        result.json(result1.rows);
+      }
+    }
+  );
+});
+
+app.get("/categories/:id/products", function(request, result) {
+  client.query(
+    // "SELECT * FROM products",
+    "SELECT products.* FROM products INNER JOIN category_products on products.id=category_products.id_product and category_products.id_category=$1::uuid",
+    // [],
+    [request.params.id],
+    function(error, result1) {
+      if (error) {
+        console.warn(error);
+      } else {
+        result.json(result1.rows);
+      }
+    }
+  );
+});
